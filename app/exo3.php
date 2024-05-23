2c7f57ed8561242d159bc8251ab20a0bd79e8e95
Sylvain Schunk <sylvain.katet@hotmail.fr>
2024-05-23 10:49:32 +0200

    exo 3 completed

--- app/exo3.php
+++ app/exo3.php
@@ -36,7 +36,14 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 1</h2>
             <p class="exercice-txt">Ordonner le tableau des prix par ordre croissant et l'afficher en détail</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    sort($prices);
+                    var_dump($prices);
+                    /*
+                    foreach ($prices as $price) {
+                        echo "<p>{$price}</p>";
+                    }*/
+                ?>
             </div>
         </section>
 
@@ -45,7 +52,21 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 2</h2>
             <p class="exercice-txt">Ajouter 1 euro à chaque prix</p>
             <div class="exercice-sandbox">
-                
+                <?php
+
+                    function addOneEuro(&$priceArray) {
+                        foreach ($priceArray as $price) {
+                            $price++;
+                            //echo "<p>{$price}</p>";
+                            //var_dump($prices);
+                        }
+                        return $priceArray;
+                    }
+
+                    $newPriceArray = addOneEuro($prices);
+                    var_dump($newPriceArray);
+
+                ?>
             </div>
         </section>
 
@@ -54,7 +75,10 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 3</h2>
             <p class="exercice-txt">Créer le tableau $store qui combine les tableaux des fruits et des prix afin d'obtenir un tableau associatif d'attribution des prix. Afficher le tableau obtenu</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    $store = array_combine($fruits, $prices);
+                    var_dump($store);
+                ?>
             </div>
         </section>
 
@@ -63,7 +87,15 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 4</h2>
             <p class="exercice-txt">Afficher dans une liste HTML le nom des fruits ayant un prix inférieur à 4 euros</p>
             <div class="exercice-sandbox">
-                
+                <ul>
+                    <?php
+                        foreach($store as $fruit => $price) {
+                            if ($price < 4) {
+                                echo "<li>{$fruit}</li>";
+                            } 
+                        }
+                    ?>
+                </ul>
             </div>
         </section>
 
@@ -72,7 +104,15 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 5</h2>
             <p class="exercice-txt">Afficher dans une liste HTML le nom des fruits ayant un prix pair</p>
             <div class="exercice-sandbox">
-                
+                <ul>
+                    <?php
+                        foreach($store as $fruit => $price) {
+                            if ($price % 2 === 0) {
+                                echo "<li>{$fruit}</li>";
+                            } 
+                        }
+                    ?>
+                </ul>
             </div>
         </section>
 
@@ -81,7 +121,18 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 6</h2>
             <p class="exercice-txt">Composer un panier de fruits ne dépassant pas 12 euros, en sélectionnant chaque fruit dans l'ordre actuel.</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    $maxPrice = 0;
+                    foreach($store as $fruit => $price) {
+                        $maxPrice+= $price;
+                        if ($maxPrice <= 12) {
+                            $cart[] = $fruit;
+                            $cartPrice[] = $price;
+                        }
+                    }
+                    $cartAndPrice = array_combine($cart, $cartPrice);
+                    var_dump($cartAndPrice);
+                ?>
             </div>
         </section>
 
@@ -90,7 +141,15 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 7</h2>
             <p class="exercice-txt">En reprenant le prix total du panier constitué à la question précédente, appliquez-lui une taxe de 18%. Afficher le total taxe comprise.</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                $totalPrice = 0;
+                foreach ($cartAndPrice as $price) {
+                    $totalPrice+= $price;
+                }
+                $vatPrice = $totalPrice + $totalPrice * 18 / 100;
+                echo "<p>Le total taxe comprise est de {$vatPrice}€.</p>"    
+                //var_dump($vatPrice);
+                ?>
             </div>
         </section>
 
@@ -99,7 +158,10 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 8</h2>
             <p class="exercice-txt">Ajouter au tableau $store le fruit "kiwi" pour un prix de 1,50 € puis afficher le tableau complet</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    $store["kiwi"] = 1.5;
+                    var_dump($store);
+                ?>
             </div>
         </section>
 
@@ -115,7 +177,10 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 9</h2>
             <p class="exercice-txt">Ajouter les nouveaux fruits du tableau $newFruits au tableau $store</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    $store = array_merge($store, $newFruits);
+                    var_dump ($store);
+                ?>
             </div>
         </section>
 
@@ -124,7 +189,15 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 10</h2>
             <p class="exercice-txt">Afficher le nom et le prix du fruit le moins cher</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    foreach ($store as $fruit => $price) {
+                        if (!isset($minPrice) || $price < $minPrice) {
+                            $minPrice = $price;
+                            $cheapestFruit = $fruit;
+                        }
+                    }
+                    echo "<p>Le fruit le moins cher est le {$cheapestFruit} : {$minPrice}€.</p>";
+                ?>
             </div>
         </section>
 
@@ -133,7 +206,15 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 11</h2>
             <p class="exercice-txt">Afficher les noms et le prix des fruits les plus chers</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    foreach ($store as $fruit => $price) {
+                        if (!isset($highestPrice) || $price > $highestPrice) {
+                            $highestPrice = $price;
+                            $mostExpensiveFruit = $fruit;
+                        }
+                    }
+                    echo "<p>Le fruit le plus cher est le {$mostExpensiveFruit} : {$highestPrice}€.</p>";
+                ?>
             </div>
         </section>
     </div>
--- app/index.php
+++ app/index.php
@@ -34,7 +34,10 @@
             <?php
                 $firstname = "Michel";
                 $score = 327;
+                echo "<p>{$firstname} a obtenu {$score} points à cette partie.</p>";
             ?>
+            <!-- Autre méthode : préférable pour sortir le PHP du HTML : -->
+            <p><?=$firstname?> a obtenu <?=$score?> points à cette partie.</p>
             </div>
         </section>
 
@@ -51,8 +54,14 @@
                 $priceProduct2 = 2.90;
                 $nameProduct3 = "potion";
                 $priceProduct3 = 5.20;
+                echo "<ul>
+                        <li>{$nameProduct1} {$priceProduct1}</li>
+                        <li>{$nameProduct2} {$priceProduct2}</li>
+                        <li>{$nameProduct3} {$priceProduct3}</li>
+                    </ul>";
             ?>
             </div>
+            
         </section>
 
         <!-- QUESTION 3 -->
@@ -64,6 +73,9 @@
                 $quantityProduct1 = 1;
                 $quantityProduct2 = 10;
                 $quantityProduct3 = 4;
+                $totalPrice = $priceProduct1 * $quantityProduct1 + $priceProduct2 * $quantityProduct2 + $priceProduct3 * $quantityProduct3;
+                $reducedPrice = $totalPrice - $totalPrice * 10 / 100 ;
+                echo "<p>Le montant total de la commande est de $reducedPrice</p>";
             ?>
             </div>
         </section>
@@ -74,7 +86,10 @@
             <h2 class="exercice-ttl">Question 4</h2>
             <p class="exercice-txt">Affichez le prix le plus élevé des 3 produits ci-dessus.</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    $maxPrice = max($priceProduct1, $priceProduct2, $priceProduct3);
+                    echo "$maxPrice";
+                ?>
             </div>
         </section>
 
@@ -88,7 +103,13 @@
             <h2 class="exercice-ttl">Question 5</h2>
             <p class="exercice-txt">Affichez dans une liste HTML le nom des produits de la question 2 qui sont présents dans la phrase : "<?= $text1 ?>"</p>
             <div class="exercice-sandbox">
-                
+                <ul>
+                    <?php
+                        if(str_contains($text1, $nameProduct1)) echo "<li>$nameProduct1</li>";
+                        if(str_contains($text1, $nameProduct2)) echo "<li>$nameProduct2</li>";
+                        if(str_contains($text1, $nameProduct3)) echo "<li>$nameProduct3</li>";
+                    ?>
+                </ul>
             </div>
         </section>
 
@@ -97,18 +118,26 @@
             <h2 class="exercice-ttl">Question 6</h2>
             <p class="exercice-txt">Parmis les scores suivants, affichez le prénom des joueurs qui ont obtenus entre 50 et 150 points.</p>
             <div class="exercice-sandbox">
-            <?php
-                $namePlayer1 = "Tim";
-                $scorePlayer1 = 67;
-                $namePlayer2 = "Morgan";
-                $scorePlayer2 = 198;
-                $namePlayer3 = "Hamed";
-                $scorePlayer3 = 21;
-                $namePlayer4 = "Camille";
-                $scorePlayer4 = 134;
-                $namePlayer5 = "Kevin";
-                $scorePlayer5 = 103;
-            ?>
+                <ul>
+                    <?php
+                        $namePlayer1 = "Tim";
+                        $scorePlayer1 = 67;
+                        $namePlayer2 = "Morgan";
+                        $scorePlayer2 = 198;
+                        $namePlayer3 = "Hamed";
+                        $scorePlayer3 = 21;
+                        $namePlayer4 = "Camille";
+                        $scorePlayer4 = 134;
+                        $namePlayer5 = "Kevin";
+                        $scorePlayer5 = 103;
+                        
+                        if ($scorePlayer1 >= 50 && $scorePlayer1 <= 150) echo "<li>$namePlayer1</li>";
+                        if ($scorePlayer2 >= 50 && $scorePlayer2 <= 150) echo "<li>$namePlayer2</li>";
+                        if ($scorePlayer3 >= 50 && $scorePlayer3 <= 150) echo "<li>$namePlayer3</li>";
+                        if ($scorePlayer4 >= 50 && $scorePlayer4 <= 150) echo "<li>$namePlayer4</li>";
+                        if ($scorePlayer5 >= 50 && $scorePlayer5 <= 150) echo "<li>$namePlayer5</li>";
+                    ?>
+                </ul>
             </div>
         </section>
 
@@ -118,7 +147,29 @@
             <h2 class="exercice-ttl">Question 7</h2>
             <p class="exercice-txt">En réutilisant les scores de la question précédente, afficher le nom du joueur ayant obtenu le plus grand score.</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    if (!isset($scoreMax) || $scorePlayer1 > $scoreMax) {
+                        $scoreMax = $scorePlayer1;
+                        $winner = $namePlayer1;
+                    }
+                    if ($scorePlayer2 > $scoreMax) {
+                        $scoreMax = $scorePlayer2;
+                        $winner = $namePlayer2;
+                    }
+                    if ($scorePlayer3 > $scoreMax) {
+                        $scoreMax = $scorePlayer3;
+                        $winner = $namePlayer3;
+                    }
+                    if ($scorePlayer4 > $scoreMax) {
+                        $scoreMax = $scorePlayer4;
+                        $winner = $namePlayer4;
+                    }
+                    if ($scorePlayer5 > $scoreMax) {
+                        $scoreMax = $scorePlayer5;
+                        $winner = $namePlayer5;
+                    }
+                    echo $winner;
+                ?>
             </div>
         </section>
 
@@ -128,6 +179,9 @@
             <h2 class="exercice-ttl">Question 8</h2>
             <p class="exercice-txt">Affichez le prénom du joueur le plus long en nombre de caractères.</p>
             <div class="exercice-sandbox">
+                <?php
+
+                ?>
                 
             </div>
         </section>
@@ -145,7 +199,16 @@
             </ul>
             <p class="exercice-txt">Afficher la valeur de cette variable avec tous les détails.</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    $players = [
+                                'Tim' => ["score" => 67, "age" => 25],
+                                'Morgan' => ["score" => 198, "age" => 34],
+                                'Hamed' => ["score" => 21, "age" => 27],
+                                'Camille' => ["score" => 134, "age" => 47],
+                                'Kevin' => ["score" => 103, "age" => 31]
+                    ];
+                    var_dump($players);
+                ?>
             </div>
         </section>
 
@@ -154,7 +217,15 @@
             <h2 class="exercice-ttl">Question 10</h2>
             <p class="exercice-txt">Afficher le prénom et l'âge du joueur le plus jeune dans une phrase dans une balise HTML P.</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                    foreach($players as $player => $array) {
+                        if (!isset($minAge) || $array["age"] < $minAge) {
+                            $minAge = $array["age"];
+                            $youngerPlayer = $player;
+                        }
+                    }
+                    echo "<p>Le plus jeune joueur est {$youngerPlayer}, {$minAge} ans.</p>";
+                ?>
             </div>
         </section>
     </div>
